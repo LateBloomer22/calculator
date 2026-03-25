5fc0f2350b4639a2090b8db8f25932445a01191d
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-25 15:02:36 +0530

    Added basic calculator function

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 let operator = "";
+let result = 0;
+let currentInput = 0;
+let previousInput = 0;
+let inputString = "";
 
 function getOperator(button) {
     operator = button.value;
-    //alert(operator);
+}
+
+function calculate(prev, operator, curr) {
+    switch (operator) {
+        case '+': return prev + curr; break;
+        case '-': return prev - curr; break;
+        case '*': return prev * curr; break;
+        case '/': 
+        if (curr == 0) {
+            return "Division by 0 not allowed";
+        } else {
+            return prev / curr ;
+        }
+    }
 }
