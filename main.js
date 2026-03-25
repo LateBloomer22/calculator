9196a33ecc686388ca8e6c8c37f879b0fec157eb
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-25 17:46:51 +0530

    Added del function to delete last entry in input

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 let operator = "";
 let result = 0;
-let currentInput = 0;
-let previousInput = 0;
-let inputString = "";
+let currentInput = "";
+let previousInput = "";
+let outputString = "";
 
 function getOperator(button) {
     operator = button.value;
 }
 
+
+function del() {
+    if (currentInput.length != 0 ) {
+        currentInput = currentInput.slice(0, -1);
+    } else {
+        return;
+    }
+};
+
+
+// function input(button){
+
+// }
+
 function calculate(prev, operator, curr) {
+    prev = Number(prev);
+    curr = Number(curr);
     switch (operator) {
         case '+': return prev + curr; break;
         case '-': return prev - curr; break;
